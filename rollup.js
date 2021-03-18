530fb98f007b3c70db9d267fa7a74ad61623b505
benjozork <benjamin.dupont.984@gmail.com>
2021-03-18 03:24:40 -0400

    feat: introduce more configurability

--- rollup.js
+++ rollup.js
@@ -1,42 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 FlyByWire Simulations
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 'use strict';
 
 const fs = require('fs');
-const { paramCase } = require('change-case');
+const { paramCase } = require("change-case");
 const [html, js] = require('./templates.js');
 
 const trim = (text) => `${text.trimStart().trimEnd()}\n`;
 
-// The bundle code contains `$`, which is a special character
-// in JS replace and replaceAll, so we can't use those
-const replace = (s, search, replacement) => s.split(search).join(replacement);
-
 module.exports = ({
-    name, config, imports = [], outputDir, getCssBundle,
+    name, instrumentDir = name, elementName, config, imports = [], outputDir, getCssBundle,
 }) => ({
-    name: 'template',
+    name: 'msfs',
     writeBundle(_config, bundle) {
         const { code: jsCode } = bundle[`${name}-gen.js`];
         const cssCode = getCssBundle();
 
-        const nameKebab = paramCase(name.toLowerCase());
+        if (config.isInteractive === undefined) {
+            console.warn('(rollup-plugin-msfs): config.isInteractive not provided, defaulting to false');
+            config.isInteractive = false;
+        }
 
-        const process = (s) => {
-            let result = s;
-            result = replace(result, 'INSTRUMENT_NAME_LOWER', nameKebab);
-            result = replace(result, 'INSTRUMENT_NAME', name);
-            result = replace(result, 'INSTRUMENT_BUNDLE', jsCode);
-            result = replace(result, 'INSTRUMENT_STYLE', cssCode);
-            result = replace(result, 'INSTRUMENT_IS_INTERACTIVE', config.isInteractive || false);
-            return result;
-        };
-
-        // Replace stuff in both the HTML and JS
-        const templateHtml = process(html(imports));
-        const templateJs = process(js);
+        const processedHtml = html(name, instrumentDir, imports, cssCode, jsCode);
+        const processedJs = js(name, config.isInteractive, elementName || paramCase(name));
 
         // Write output
-        fs.mkdirSync(`${outputDir}/${name}`, { recursive: true });
-        fs.writeFileSync(`${outputDir}/${name}/template.html`, trim(templateHtml));
-        fs.writeFileSync(`${outputDir}/${name}/template.js`, trim(templateJs));
+        fs.mkdirSync(`${outputDir}/${instrumentDir}`, { recursive: true });
+        fs.writeFileSync(`${outputDir}/${instrumentDir}/template.html`, trim(processedHtml));
+        fs.writeFileSync(`${outputDir}/${instrumentDir}/template.js`, trim(processedJs));
     },
 });
--- templates.js
+++ templates.js
@@ -1,58 +1,66 @@
 /*
- * A32NX
- * Copyright (C) 2020-2021 FlyByWire Simulations and its contributors
+ * MIT License
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright (c) 2021 FlyByWire Simulations
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 'use strict';
 
-const html = (imports) => `
-<script type="text/html" id="A32NX_INSTRUMENT_NAME_TEMPLATE">
-    <div id="A32NX_REACT_MOUNT">
+const { paramCase } = require('change-case');
+
+const html = (name, outputDir, imports, css, js) => `
+<script type="text/html" id="${name}_TEMPLATE">
+    <div id="MSFS_REACT_MOUNT">
         <h1>If you're seeing this, React didn't load.</h1>
     </div>
 
-    <script id="A32NX_BUNDLED_LOGIC">
+    <script id="${name}_BUNDLED_LOGIC">
         // Although this is a script tag, because it is a child of a custom element,
         // it will not execute. Inside \`template.js\`, we read the contents of this script
         // element, then create a new script element and append it to the document body.
         // We embed it into this file, even though it is useless, because this file can
         // be hot-reloaded, but \`template.js\` cannot.
         /* eslint-disable */
-        INSTRUMENT_BUNDLE
+        ${js}
     </script>
-    <style id="A32NX_BUNDLED_STYLE">
+    <style id="${name}_BUNDLED_STYLE">
         /* same deal as above */
-        INSTRUMENT_STYLE
+        ${css}
     </style>
 </script>
 
 ${imports.map(i => 
     `<script type="text/html" import-script="${i}" import-async="false"></script>\n`
 )}
-<script type="text/html" import-script="/Pages/VCockpit/Instruments/generated/INSTRUMENT_NAME/template.js" import-async="false"></script>
+<script type="text/html" import-script="/Pages/VCockpit/Instruments/${outputDir}/template.js" import-async="false"></script>
 `;
 
-const js = `
+const js = (name, isInteractive, elementName) => `
 'use strict';
 
 /* global BaseInstrument */
 /* global registerInstrument */
 
 // eslint-disable-next-line camelcase
-class A32NX_INSTRUMENT_NAME_Logic extends BaseInstrument {
+class ${name}_Logic extends BaseInstrument {
     constructor() {
         super();
         // eslint-disable-next-line no-underscore-dangle
@@ -67,12 +75,12 @@ class A32NX_INSTRUMENT_NAME_Logic extends BaseInstrument {
     }
 
     get templateID() {
-        return 'A32NX_INSTRUMENT_NAME_TEMPLATE';
+        return '${name}_TEMPLATE';
     }
 
     get isInteractive() {
         // eslint-disable-next-line
-        return INSTRUMENT_IS_INTERACTIVE;
+        return ${isInteractive};
     }
 
     get IsGlassCockpit() {
@@ -84,13 +92,13 @@ class A32NX_INSTRUMENT_NAME_Logic extends BaseInstrument {
 
         // This is big hack, see \`template.html\`.
         {
-            const code = document.getElementById('A32NX_BUNDLED_STYLE').innerHTML;
+            const code = document.getElementById('${name}_BUNDLED_STYLE').innerHTML;
             const style = document.createElement('style');
             style.innerHTML = code;
             document.head.appendChild(style);
         }
         {
-            const code = document.getElementById('A32NX_BUNDLED_LOGIC').innerHTML;
+            const code = document.getElementById('${name}_BUNDLED_LOGIC').innerHTML;
             const script = document.createElement('script');
             script.innerHTML = code;
             document.body.appendChild(script);
@@ -109,7 +117,7 @@ class A32NX_INSTRUMENT_NAME_Logic extends BaseInstrument {
     }
 }
 
-registerInstrument('a32nx-INSTRUMENT_NAME_LOWER-element', A32NX_INSTRUMENT_NAME_Logic);
+registerInstrument('${elementName || paramCase(name)}-element', ${name}_Logic);
 `;
 
 module.exports = [html, js];
